96b6570dd5f616972527322031037bfb1311f31c
Warawich Sureepitak <warawich@gmail.com>
2018-12-04 14:25:56 +0700

    Initail File

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    //res.statusCode = 200;
+    //res.setHeader('Content-Type', 'text/plain');
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<title>http - Node.js Manual &amp; Documentation</title>');
+      res.write('Hello World Sawasdee krub\n');
+      res.end();
+      });
+    
+server.listen(port, hostname, () =>  {
+     console.log(`Server running at http://${hostname}:${port}/`);
+});							 
+
+
