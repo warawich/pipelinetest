c00f6441249ccfc4dd273524dc8b3479d4a1e6c6
Warawich Sureepitak <warawich@gmail.com>
2018-12-21 17:58:42 +0700

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ ssh-add /root/.ssh/id_rsa
 
 tar czf deploy.tgz app.js test.robot
 scp -pr deploy.tgz root@192.168.1.125:/root/deploy/.
+ssh root@192.168.1.125 tar xzf /root/deploy/deploy.tgz -C  /root/deploy
+ssh root@192.168.1.125 rm -rvf  /root/deploy/deploy.tgz
 eval `ssh-agent -k`
