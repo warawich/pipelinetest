a2edd5d3766076b56996fa38d5be69b4e093ec6d
Warawich Sureepitak <warawich@gmail.com>
2018-12-21 16:52:50 +0700

    add tgz

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 eval `ssh-agent`
 ssh-add ${PWD}/.ssh/id_rsa
 
-tar czf deploy.sh app.js test.robot
-scp -pr deploy.sh root@192.168.1.125:/root/deploy/.
-
+tar czf deploy.tgz app.js test.robot
+scp -pr deploy.tgz root@192.168.1.125:/root/deploy/.
 eval `ssh-agent -k`
